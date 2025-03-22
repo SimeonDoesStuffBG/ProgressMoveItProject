8a7ca3946fc71dbad90a34b22c30142d65297e48
SimeonDoesStuffBG <simeondoesstuff@gmail.com>
2025-03-22 16:58:48 +0200

    Created .NET Project

---
+++ ProgressTestApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProgressTestApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
